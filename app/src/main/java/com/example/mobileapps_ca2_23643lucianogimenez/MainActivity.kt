837df598dc74dfd19b32cfaaa55a124a12fc8976
Agustin Pellegri <agustinpellegri@Agustins-MacBook-Air.local>
2022-03-27 18:44:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mobileapps_ca2_23643lucianogimenez/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mobileapps_ca2_23643lucianogimenez
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
